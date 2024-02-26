cd331f42db9dda41b2d7dda3ef37adf9ecc70412
j.pinto <j.pinto@pawa-consulting.com>
2024-02-26 10:30:03 +0100

    add Model Tag + Tag DTO + TagController

---
+++ API_RKonnect/Controllers/TagController.cs
@@ -0,0 +1,43 @@
+﻿using API_RKonnect.Dto;
+using API_RKonnect.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_RKonnect.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+
+        [Authorize]
+        [HttpPost("add")]
+        public async Task<ActionResult<Tag>> AddTag(TagDto request, [FromServices] DataContext context)
+        {
+            if (string.IsNullOrWhiteSpace(request.Title) && string.IsNullOrWhiteSpace(request.Icon))
+            {
+                return BadRequest("Tag title or icon cannot be empty.");
+            }
+
+            var tagExists = await context.Tag.FirstOrDefaultAsync(t => t.Title == request.Title);
+            if (tagExists != null)
+            {
+                return BadRequest("This tag already exists.");
+            }
+
+            var tag = new Tag
+            {
+                Title = request.Title,
+                Icon = request.Icon
+            };
+
+            context.Tag.Add(tag);
+            await context.SaveChangesAsync();
+
+            return Ok(tag);
+        }
+
+    }
+}
---
+++ API_RKonnect/Dto/TagDto.cs
@@ -0,0 +1,8 @@
+﻿namespace API_RKonnect.Dto
+{
+    public class TagDto
+    {
+        public string? Title { get; set; }
+        public string? Icon { get; set; }
+    }
+}
