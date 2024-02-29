b7c23303b7b88725920423e1b5a8fb862b4af987
j.pinto <j.pinto@pawa-consulting.com>
2024-02-29 14:34:18 +0100

    add UserInvitationModel

--- API_RKonnect/Models/Invitation.cs
+++ API_RKonnect/Models/Invitation.cs
@@ -1,6 +1,17 @@
-﻿namespace API_RKonnect.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_RKonnect.Models
 {
-    public class Class
+    public class Invitation
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public Restaurant Restaurant { get; set; } = new Restaurant();
+        [Required]
+        public int UserMax { get; set; }
+        [Required]
+        public DateTime InvitationDate { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
---
+++ API_RKonnect/Models/UserInvitation.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_RKonnect.Models
+{
+    public class UserInvitation
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int InvitationId { get; set; }
+        [Required]
+        public Invitation Invitation { get; set; } = new Invitation();
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public User User { get; set; } = new User();
+    }
+}
