9fdeeaf3eecd6b8f0ffb0f28e279a399c7512fac
j.pinto <j.pinto@pawa-consulting.com>
2024-02-29 09:13:16 +0100

    change icon from TagModel to FoodModel + update UserController routing

--- API_RKonnect/Models/Food.cs
+++ API_RKonnect/Models/Food.cs
@@ -6,6 +6,7 @@ namespace API_RKonnect.Models
     {
         [Key]
         public int Id { get; set; }
-        public string? Name { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
     }
 }
