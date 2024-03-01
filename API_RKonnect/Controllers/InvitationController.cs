bda07b557cf578eec113a65e6b916f96a420ebfb
j.pinto <j.pinto@pawa-consulting.com>
2024-03-01 12:15:12 +0100

    add join + getById for Invitation

--- API_RKonnect/Controllers/InvitationController.cs
+++ API_RKonnect/Controllers/InvitationController.cs
@@ -21,6 +21,105 @@ namespace API_RKonnect.Controllers
         }
 
         [Authorize]
+        [HttpGet("getById/{userId}")]
+        public IActionResult GetInvitationById(int userId)
+        {
+            var currentUser = _context.Utilisateur.FirstOrDefault(u => u.Id == userId);
+            if (currentUser != null)
+            {
+                 var invitations = _context.UserInvitation
+                .Where(ui => ui.UserId == userId)
+                .Include(ui => ui.Invitation)
+                .Select(ui => new GetInvitationInfoDto
+                {
+                    InvitationId = ui.InvitationId,
+                    IsAccepted = ui.IsAccepted,
+                    Guests = _context.UserInvitation
+                                .Where(u => u.InvitationId == ui.InvitationId && u.UserId != userId)
+                                .Select(u => new PublicUserDto
+                                {
+                                    Id = u.UserId,
+                                    Pseudo = u.User.Pseudo,
+                                    Avatar = u.User.Avatar,
+                                    Email = u.User.Email,
+                                    CreatedAt = u.User.CreatedAt,
+                                    UpdatedAt = u.User.UpdatedAt
+                                }).ToList()
+                }).ToList();
+
+                if (invitations == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(invitations);
+            }
+            else
+            {
+                return BadRequest("User not found.");
+            }
+        }
+
+        [Authorize]
+        [HttpGet("join/{invitationId}")]
+        public async Task<IActionResult> JoinInvitation(int invitationId)
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var invitationExist = await _context.Invitation.FirstOrDefaultAsync(ie => ie.Id == invitationId);
+
+            //check if places are available
+            var invitationUserMax = invitationExist.UserMax;
+            var countAcceptedInvitations = await _context.UserInvitation
+                .Where(invitation => invitation.InvitationId == invitationId && invitation.IsAccepted == true)
+                .CountAsync();
+
+            if (userId != null || invitationExist != null)
+            {
+                int userIdInt = int.Parse(userId);
+                var user = await _context.Utilisateur.FirstOrDefaultAsync(u => u.Id == userIdInt);
+                bool reservationExists = _context.UserInvitation.Any(r => r.InvitationId == invitationId && r.UserId == userIdInt);
+
+                if (user != null)
+                {
+                    if(reservationExists != true)
+                    {
+                        if (countAcceptedInvitations < invitationUserMax)
+                        {
+                            var newUserInvitation = new UserInvitation
+                            {
+                                InvitationId = invitationExist.Id,
+                                Invitation = invitationExist,
+                                UserId = userIdInt,
+                                User = user,
+                                IsAccepted = false
+                            };
+                            _context.UserInvitation.Add(newUserInvitation);
+                            await _context.SaveChangesAsync();
+
+                            return Ok("Join success !");
+                        }
+                        else
+                        {
+                            return BadRequest("There are no more places available.");
+                        }
+                    }
+                    else
+                    {
+                        return BadRequest("Reservation already exists for this user.");
+                    } 
+                }
+                else
+                {
+                    return BadRequest("User not found.");
+                }
+            }
+            else
+            {
+                return BadRequest("User id or invitation not found.");
+            }
+        }
+
+        [Authorize]
         [HttpPost("add")]
         public async Task<ActionResult<Invitation>> AddInvitation(InvitationDto request)
         {
---
+++ API_RKonnect/Dto/GetInvitationInfoDto.cs
@@ -0,0 +1,11 @@
+﻿using API_RKonnect.Models;
+
+namespace API_RKonnect.Dto
+{
+    public class GetInvitationInfoDto
+    {
+        public int InvitationId { get; set; }
+        public bool IsAccepted { get; set; }
+        public List<PublicUserDto> Guests { get; set; }
+    }
+}
