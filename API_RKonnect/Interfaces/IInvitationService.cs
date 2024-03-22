795036eee5dfc6f6e2d4994907708ba89406c72d
j.pinto <j.pinto@pawa-consulting.com>
2024-03-22 11:02:45 +0100

    add getAll invitation

--- API_RKonnect/Interfaces/IInvitationService.cs
+++ API_RKonnect/Interfaces/IInvitationService.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 public interface IInvitationService
 {
+    IActionResult getAll([FromServices] DataContext context);
     IActionResult GetInvitationById(int userId, [FromServices] DataContext context);
     Task<IActionResult> JoinInvitation(int invitationId, int userId, [FromServices] DataContext context);
     Task<ActionResult<Invitation>> AddInvitation(InvitationDto request, int userId, [FromServices] DataContext context);
